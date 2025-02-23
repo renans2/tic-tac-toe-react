81307d3178f967fae3efb401ac10e13c28d02a4b
Renan Silva <renansupermanox@gmail.com>
2025-02-23 16:14:57 +0000

    more styles

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ export default function App() {
                 gameOver &&
                 <button 
                     onClick={handleReset} 
-                    className="transition duration-200 hover:bg-orange-400 font-medium text-[min(5vw,5vh)] cursor-pointer mb-2 rounded-[min(3vw,3vh)] bg-amber-50 w-full h-[min(10vw,10vh)] border-2 border-amber-950"
+                    className="transition duration-200 hover:bg-orange-400 font-medium text-[min(5vw,5vh)] cursor-pointer mb-2 rounded-[min(3vw,3vh)] bg-white w-full h-[min(10vw,10vh)] border-2 border-amber-950"
                 >
                     Play Again
                 </button> 
--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -20,18 +20,23 @@ export default function Square({ row, col }){
         if(gameOver === false)
             setText();
     }, [gameOver]);
+
+    const greenSquare = () => {
+        return grid.current.checkWinner() && grid.current.checkIfInWinnerLine(row, col);
+    }
     
     return(
         <button 
             onClick={handlePlay} 
             disabled={gameOver || text !== undefined}
             className={
-                (grid.current.checkWinner() && 
-                 grid.current.checkIfInWinnerLine(row, col) 
-                    ? "bg-green-500" 
-                    : "bg-blue-950")
-                    // + gameOver || text !== undefined ? " cursor-not-allowed" : " cursor-pointer" 
-                    + " w-full h-full relative text-[min(20vw,20vh)] rounded-[10%]"
+                (greenSquare()
+                ? "bg-green-500 cursor-not-allowed" 
+                : gameOver || text !== undefined 
+                ? "bg-blue-950 cursor-not-allowed" 
+                : "bg-blue-900 transition duration-300 hover:bg-blue-700 cursor-pointer")
+                
+                + " w-full h-full relative text-[min(20vw,20vh)] rounded-[10%]"
             }
         >
             <span className="absolute text-white top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2">{text}</span>
